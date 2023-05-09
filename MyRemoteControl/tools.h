6ef4b15143af4730e04990859a796cee6709515d
Кирилл Морозов <kira.morozovkirill.2018.aprel@gmail.com>
2023-05-09 12:17:15 +0300

    main

--- MyRemoteControl/tools.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <Windows.h>
-#include "types.h"
-
-Size2i GetScreenSize() {
-	Size2i size = {GetSystemMetrics(SM_CXSCREEN), GetSystemMetrics(SM_CYSCREEN)};
-	return size;
-}
